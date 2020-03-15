f1c69a501e296208860f145023b4e6cd7de2bdc8
Kimaru thagana <macbuntu@localhost.localdomain>
2020-03-15 20:52:17 +0300

    register single simple model

--- nosql/admin.py
+++ nosql/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from djongo.admin import admin
+from .models import *
 # Register your models here.
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'views')
+
+admin.site.register(BlogPost, BlogPostAdmin)
--- nosql/models.py
+++ nosql/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+# simple blog application
+class BlogPost(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    views = models.IntegerField(default=0)
+    created_on = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updatedOn  = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
