631311ad7f7d465bf2a673b8b3bc308a4d3148e0
Kimaru thagana <macbuntu@localhost.localdomain>
2020-03-16 13:24:33 +0300

    add post method

--- nosql/views.py
+++ nosql/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import *
 from .serializers import *
 # Create your views here.
@@ -17,3 +18,10 @@ class AllPosts (APIView):
             all_posts = BlogPostSerializer(posts, many=True).data
             response = {'results': all_posts}
             return Response(response)
+
+    def post(self, request, format=None):
+        serializer = BlogPostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
